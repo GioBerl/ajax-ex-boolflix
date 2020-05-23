1cd4d622176ee78a9a5de900737617e09dea17d5
Gio <giovanni.berl1@gmail.com>
2020-05-23 12:01:01 +0200

    aggiunte diverse funzioni e migliorato un po la griglia

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 $(document).ready(function () {
     $("#search-button").click(function () {
+        sendRequest();
+        clearInput();
+    });
+
+    $("input#search-text").keyup(function (event) {
+        if (event.key == "Enter") {
+            sendRequest();
+            clearInput();
+        }
+    });
+
+    function clearInput() {
+        $("input#search-text").val("");
+    }
+
+    function clearMain() {
+        $(".card-list").html("");
+    }
+
+    function sendRequest() {
         //mi assicuro che ci sia scritto qualcosa nell'input
         if (!$("input#search-text").val()) {
             alert("inserisci almeno un carattere per la ricerca");
         } else {
             //pulisco il main nel caso avessi gia' effettuato una richiesta
-            $(".card-list").html("");
+            clearMain();
             //leggo il valore dell'input e lo uso nella query della request
             var film = $("input#search-text").val();
             $.ajax({
@@ -19,7 +39,7 @@ $(document).ready(function () {
                 success: function (response) {
                     // salvo l'array di dati di ritorno
                     var results = response.results; //[{...},{...},{...}...]
-                    //passo i risultati alla funzione cicleAndPrint che si occupa di ciclare l'array e stampare le liste singole liste
+                    //passo i risultati alla funzione cicleAndPrint che si occupa di ciclare l'array e stampare le singole liste
                     cicleAndPrint(results);
                 },
                 error: function () {
@@ -27,14 +47,14 @@ $(document).ready(function () {
                 },
             });
         }
-    });
+    }
 
     function cicleAndPrint(risultati) {
-        risultati.forEach(function (info) {
-            var title = info.title;
-            var orTitle = info.original_title;
-            var orLanguage = info.original_language;
-            var averageVote = info.vote_average;
+        risultati.forEach(function (infos) {
+            var title = infos.title;
+            var orTitle = infos.original_title;
+            var orLanguage = infos.original_language;
+            var averageVote = infos.vote_average;
             if (title == orTitle) {
                 //metto un segno a orTitle per indicare che e' uguale a title
                 orTitle = `"`;
@@ -48,6 +68,4 @@ $(document).ready(function () {
             </ul>`);
         });
     }
-
-    function isEqual() {}
 });
