8040612f97a2d2c32bc4b57f8e334c41836699e5
Gio <giovanni.berl1@gmail.com>
2020-05-25 12:24:32 +0200

    aggiunto titolo ricerca e handlebars

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var source = $("#card-template").html();
+    var templateFunc = Handlebars.compile(source);
     $("#search-button").click(function () {
         sendRequest();
         clearInput();
@@ -21,9 +23,7 @@ $(document).ready(function () {
 
     function sendRequest() {
         //mi assicuro che ci sia scritto qualcosa nell'input
-        if (!$("input#search-text").val()) {
-            alert("inserisci almeno un carattere per la ricerca");
-        } else {
+        if ($("input#search-text").val().trim()) {
             //pulisco il main nel caso avessi gia' effettuato una richiesta
             clearMain();
             //leggo il valore dell'input e lo uso nella query della request
@@ -37,6 +37,10 @@ $(document).ready(function () {
                     language: "it",
                 },
                 success: function (response) {
+                    // inserisco il testo cercato dall'utente nel titolo della pagina
+                    $("#ricerca-utente").text(film);
+                    // visualizzo il titolo della pagina
+                    $(".title-search").addClass("visible");
                     // salvo l'array di dati di ritorno
                     var results = response.results; //[{...},{...},{...}...]
                     //passo i risultati alla funzione cicleAndPrint che si occupa di ciclare l'array e stampare le singole liste
@@ -46,26 +50,29 @@ $(document).ready(function () {
                     alert("errore");
                 },
             });
+        } else {
+            alert("inserisci almeno un carattere");
         }
     }
 
     function cicleAndPrint(risultati) {
+        //preparo i dati per il template
         risultati.forEach(function (infos) {
-            var title = infos.title;
-            var orTitle = infos.original_title;
-            var orLanguage = infos.original_language;
-            var averageVote = infos.vote_average;
-            if (title == orTitle) {
+            //recupero i dati di ogni elemento dell'array
+            var context = {
+                title: infos.title,
+                orTitle: infos.original_title,
+                orLanguage: infos.original_language,
+                averageVote: infos.vote_average,
+            };
+            if (context.title == context.orTitle) {
                 //metto un segno a orTitle per indicare che e' uguale a title
-                orTitle = `"`;
+                context.orTitle = `"`;
             }
-            $(".card-list").append(`
-            <ul class="single-list">
-                <li>${title}</li>
-                <li>${orTitle}</li>
-                <li>${orLanguage}</li>
-                <li>${averageVote}</li>
-            </ul>`);
+            //li compilo
+            var htmlCard = templateFunc(context);
+            //e li aggiungo al main
+            $(".card-list").append(htmlCard);
         });
     }
 });
