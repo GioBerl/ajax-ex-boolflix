f18190b78d647ca1eae07ce9c60c73803f98b260
Gio <giovanni.berl1@gmail.com>
2020-05-26 21:16:02 +0200

    funzioni base ok ora prova milestone 5

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
+    var apiKey = "da57a6e390d49d195b53f218c7690a55";
+    var baseUrl = "https://api.themoviedb.org/3/";
+
+    var imgUrl = "https://image.tmdb.org/t/p/w185";
+
     var source = $("#card-template").html();
     var templateFunc = Handlebars.compile(source);
     $("#search-button").click(function () {
         sendRequest();
-        sendRequestTv();
         clearInput();
     });
 
     $("input#search-text").keyup(function (event) {
         if (event.key == "Enter") {
             sendRequest();
-            sendRequestTv();
             clearInput();
         }
     });
@@ -31,18 +34,19 @@ $(document).ready(function () {
             // nascondo il titolo della pagina
             $(".title-search").removeClass("visible");
             //leggo il valore dell'input e lo uso nella query della request
-            var film = $("input#search-text").val();
+            var textSearch = $("input#search-text").val();
+            //PER I FILM
             $.ajax({
                 method: "GET",
-                url: "https://api.themoviedb.org/3/search/movie",
+                url: baseUrl + "search/movie",
                 data: {
-                    api_key: "da57a6e390d49d195b53f218c7690a55",
-                    query: film,
+                    api_key: apiKey,
+                    query: textSearch,
                     language: "it",
                 },
                 success: function (response) {
                     // inserisco il testo cercato dall'utente nel titolo della pagina
-                    $("#ricerca-utente").text(film);
+                    $("#ricerca-utente").text(textSearch);
                     // visualizzo il titolo della pagina
                     $(".title-search").addClass("visible");
                     // salvo l'array di dati di ritorno
@@ -54,26 +58,13 @@ $(document).ready(function () {
                     alert("errore");
                 },
             });
-        } else {
-            alert("inserisci almeno un carattere");
-        }
-    }
-
-    function sendRequestTv() {
-        //mi assicuro che ci sia scritto qualcosa nell'input
-        if ($("input#search-text").val().trim()) {
-            // //pulisco il main nel caso avessi gia' effettuato una richiesta
-            // clearMain();
-            // // nascondo il titolo della pagina
-            // $(".title-search").removeClass("visible");
-            //leggo il valore dell'input e lo uso nella query della request
-            var tv = $("input#search-text").val();
+            //PER LE SERIE TV
             $.ajax({
                 method: "GET",
-                url: "https://api.themoviedb.org/3/search/tv",
+                url: baseUrl + "search/tv",
                 data: {
-                    api_key: "da57a6e390d49d195b53f218c7690a55",
-                    query: tv,
+                    api_key: apiKey,
+                    query: textSearch,
                     language: "it",
                 },
                 success: function (response) {
@@ -94,8 +85,9 @@ $(document).ready(function () {
     function cicleAndPrint(risultati) {
         //preparo i dati per il template
         risultati.forEach(function (infos) {
-            //recupero i dati di ogni elemento dell'array
+            //recupero i dati di ogni elemento dell'array e li passo a handlebars
             var context = {
+                // potevi utilizzare hasOwnProperty('title')
                 title: infos.title ? infos.title : infos.name,
                 orTitle: infos.original_title
                     ? infos.original_title
@@ -105,6 +97,10 @@ $(document).ready(function () {
                 orLanguage: createFlag(infos.original_language),
                 averageVote: infos.vote_average,
                 stars: createStars(infos.vote_average),
+                type: infos.title ? "film" : "serie",
+                poster: infos.poster_path
+                    ? imgUrl + infos.poster_path
+                    : "img/img-not-available.png",
             };
 
             //se i due titoli sono uguali metto un segno a orTitle per indicare che e' uguale a title
@@ -122,31 +118,26 @@ $(document).ready(function () {
         //costruisco una lista di linguaggi preimpostati corrispondenti alle immagini
         var langList = ["it", "en", "fr", "de"];
         // controllo se la lingua del film e' presente in questa lista
-        //prova uso ternary operator
         var imgFlag = langList.includes(lang)
-            ? `<img src="img/flag_${lang}.png" />`
+            ? `<img class="flag" src="img/flag_${lang}.png" />`
             : lang;
-        // sarebbe come scrivere:
-        // if (langList.includes(lang)) {
-        //     return `<img src="img/flag_${lang}.png" />`
-        // } else {
-        //     return lang
-        // }
         return imgFlag;
     }
 
     function createStars(num) {
         //trasformo il voto da 1 a 10 in un numero da uno a 5 arrotondando per eccesso
-        var ceilNumber = Math.ceil(num / 2);
+        var ceilNumber = Math.round(num / 2);
         var starsIcon = "";
-        if (num == 0) {
-            starsIcon += '<i class="far fa-star"></i>';
-        } else {
-            for (var i = 0; i < ceilNumber; i++) {
+        for (var i = 1; i <= 5; i++) {
+            // devo aggiungere una stella piena o una stella vuota?
+            if (i <= ceilNumber) {
+                // stella piena
                 starsIcon += '<i class="fas fa-star"></i>';
+            } else {
+                // stella vuota
+                starsIcon += '<i class="far fa-star"></i>';
             }
         }
-
         return starsIcon;
     }
 });
